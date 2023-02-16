73f60b3ddd431ee6ba8c15021646448debebdbe6
Paulo Henrique <ph.ouverney@gmail.com>
2023-02-16 16:12:26 -0300

    init with deploy

---
+++ README.md
@@ -0,0 +1,6 @@
+## How to run
+pip install virtualenv
+python -m virtualenv venv
+
+. ./venv/bin/activate
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='vulcan_ms_core',
+    packages=find_packages(include=['vulcan_ms_core']),
+    version='0.1.0',
+    description='Library for Vulcan Microservices',
+    author='Overnight',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
