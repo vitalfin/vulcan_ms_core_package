7300ccb6f1ffa849400a961e6452d8ae990dde23
Paulo Henrique <ph.ouverney@gmail.com>
2023-02-16 16:57:28 -0300

    fix include subpackages

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -6,3 +6,7 @@ def test_haversine():
         myfunctions.haversine(52.370216, 4.895168, 52.520008, 13.404954)
         == 945793.4375088713
     )
+
+
+def sum_two_numbers():
+    assert myfunctions.sum_two_numbers(1, 2) == 3
