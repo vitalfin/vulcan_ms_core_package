fa5c303217b7096deb665498dc39096c1b4ea137
Paulo Henrique <ph_ouverney@id.uff.br>
2023-02-17 18:57:49 -0300

    try tonuse version from environment

--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -1,4 +1,8 @@
 """Vulcan Microservice Core Library"""
 import datetime
+import os 
 
-__version__ = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M")
+version = os.getenv("VERSION")
+if version is None:
+  version = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M")
+__version__ = versio
